4f29d262e45d3f7aa9f83ee4845754e779f49006
Sujan khatri <sujan.chettri1321@gmail.com>
2025-01-30 21:26:29 +0545

    add main component with createElement and createRoot

---
+++ React/src/Component/main.jsx
@@ -0,0 +1,4 @@
+import { createElement } from 'react';
+import {createroot} from 'react-dom';
+const root =createRoot(document.getElementByID("root"))
+const reactelement =createElement("h1",null,"Hello from createElement");
