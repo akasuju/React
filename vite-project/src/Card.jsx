27ed016d764fcda644f077742bab6b5aef1e9671
Sujan khatri <sujan.chettri1321@gmail.com>
2025-02-17 22:55:57 +0545

    style: enhance Card component with improved CSS classes for image and text

--- vite-project/src/Card.jsx
+++ vite-project/src/Card.jsx
@@ -4,9 +4,9 @@ import Profile from './assets/Profile.jpg'
 function Card(){
     return(
 <div className="card">
-<img src={Profile} style={{height: "200px", width: "200px"}} alt="Profile Picture"></img>
-<h2>Sujan</h2>
-<p>I am a student learning React through react docs and BroCode</p>
+<img className='card-img' src={Profile} style={{height: "200px", width: "200px"}} alt="Profile Picture"></img>
+<h2 className='card-title'>Sujan</h2>
+<p className='card-text'>I am a student learning React through react docs and BroCode</p>
 </div>
     )
 }
